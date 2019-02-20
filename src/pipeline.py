19bf35a181e43d911d92d32d4c331e1077e5bc6b
Kai Pak <kai@kaipak.org>
2019-02-19 23:59:26 -0800

    First submission! randomforestclassifier chosen. Work on cleaning up data.

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,29 +1,23 @@
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
-
-def prep_df(df, drop_columns):
-    """ Prepare dataframe for use in ML algorithm.
-        1) We'll impute median values where needed.
-        2) One-hot encode categorical features
-        
-        parameters:
-        df - dataframe to clean up and prep for ML
-        columns (list, array) - since we generally want to avoid 
-            non-numerical columns, we'll have to specify which ones 
-            to exclude. ToDo: Make this function auto-do this.
-    """
-
-    # Impute step
-    imputer = SimpleImputer(strategy="median")
-    df_imputted = df.drop(columns=drop_columns, axis=1)
-    imputer.fit(df_imputted)
-    X = imputer.transform(df_imputted)
-    df_transformed = pd.DataFrame(X, columns=df_imputted.columns)
-
-    # One-hot encoding
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
 
 
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
 
 
-    return(df_transformed)
+    def fit(self, X, y=None):
+        return self
+
+
+    def transform(self, X):
+        return X[self.attribute_names].values
+
+def age_cats(df, cut_points, labels):
+    df["Age"] = df["Age"].fillna(-0.5)
+    df["Age_cat"] = pd.cut(df["Age"], cut_points, labels=labels)
+    return df
