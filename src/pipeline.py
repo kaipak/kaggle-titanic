bccc64100cffac46e10bc5152bc3d629c74c04e7
Kai Pak <kai@kaipak.org>
2019-02-25 22:32:38 -0800

    - Created class for dataframe pipeline - Cleaned up notebook and reordered things so it makes more sense. - Improvement to .75598 accuracy on test set!

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline, FeatureUnion
 
 
 class DataFrameSelector(BaseEstimator, TransformerMixin):
@@ -17,10 +18,24 @@ class DataFrameSelector(BaseEstimator, TransformerMixin):
     def transform(self, X):
         return X[self.attribute_names].values
 
-def age_cats(df, cut_points, labels):
-    df["Age"] = df["Age"].fillna(-0.5)
-    df["Age_cat"] = pd.cut(df["Age"], cut_points, labels=labels)
-    return df
+
+class DataFramePipeline():
+    def __init__(self, cat_attribs, num_attribs):
+        self.num_pipeline = Pipeline([
+            ('selector', DataFrameSelector(num_attribs)),
+            ('std_scaler', StandardScaler()),
+        ])
+
+        self.cat_pipeline = Pipeline([
+            ('selector', DataFrameSelector(cat_attribs)),
+            ('cat_encoder', OneHotEncoder(sparse=False)),
+        ])
+
+        self.full_pipeline = FeatureUnion(transformer_list=[
+            #("num_pipeline", self.num_pipeline),
+            ("cat_pipeline", self.cat_pipeline), 
+        ])
+
 
 def attrib_cats(df, orig_cat, new_cat, cut_points, labels):
     df[orig_cat] = df[orig_cat].fillna(-0.5)
